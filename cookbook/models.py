184afadc40b9692fdabb733414951eb2dfef8d3f
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-04-19 13:35:13 +0000

    Add style recipe home page and base

--- cookbook/models.py
+++ cookbook/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    excerp = models.CharField(max_length=50, blank=True, null=True)
+    prep_time = models.DurationField(blank=True, null=True)
+    cooking_time = models.DurationField(blank=True, null=True)
+    servings = models.IntegerField(default=1) 
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    date_posted = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
--- cookbook/views.py
+++ cookbook/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Recipe
 
-recipes = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Recipe 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Recipe 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
 
 def home(request):
     context = {
-        'recipes': recipes
+        'recipes': Recipe.objects.all()
     }
     return render(request, 'cookbook/home.html', context)
 
