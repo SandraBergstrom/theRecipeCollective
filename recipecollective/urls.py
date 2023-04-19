d15dd4815f7a0d3f21950614ea19fba16c64174f
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-04-19 10:03:19 +0000

    Make cookbook home page

--- recipecollective/urls.py
+++ recipecollective/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cookbook/', include('cookbook.urls')),
+    path('', include('cookbook.urls')),
 ]
