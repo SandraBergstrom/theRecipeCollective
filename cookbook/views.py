5c925808c0b4419dd0a9958f0d69b7289e38518c
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-04-19 11:07:12 +0000

    Add base.html and bootstrap

--- cookbook/views.py
+++ cookbook/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+recipes = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Recipe 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Recipe 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Cookbook Home</h1>')
+    context = {
+        'recipes': recipes
+    }
+    return render(request, 'cookbook/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Cookbook About</h1>')
+    return render(request, 'cookbook/about.html', {'title': 'About'})
--- recipecollective/settings.py
+++ recipecollective/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    'cookbook',
+    'cookbook.apps.CookbookConfig',
 ]
 
 MIDDLEWARE = [
