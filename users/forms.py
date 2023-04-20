5b9b3b4333e666c65809cd75ec106c6875335472
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-04-20 11:06:26 +0000

    Add first name and last name fields to registration form

--- users/forms.py
+++ users/forms.py
@@ -8,4 +8,4 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username','first_name', 'last_name', 'email', 'password1', 'password2']
