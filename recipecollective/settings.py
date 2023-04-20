8c05d723315aa7187df90dc60f311cd1f13cbb9f
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-04-20 13:43:02 +0000

    Add profile.html and require login to access profile

--- recipecollective/settings.py
+++ recipecollective/settings.py
@@ -147,6 +147,7 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 LOGIN_REDIRECT_URL = 'cookbook-home'
+LOGIN_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
+
 
 
 def register(request):
@@ -18,3 +20,7 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
