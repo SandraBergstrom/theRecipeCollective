139165d27345e79f930b469fffae2d34f76aeed1
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-04-20 10:14:21 +0000

    Add email field to registration form

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Registration form
 - Password validation - if not a match, can't register
 - Flash message shows to confirm user is created when validation is ok. Yes
 - Check in django admin panel if user was actually created. Yes, it was
+- Check if email was saved  - yes
--- users/views.py
+++ users/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -15,6 +15,6 @@ def register(request):
                 )
             return redirect('cookbook-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
