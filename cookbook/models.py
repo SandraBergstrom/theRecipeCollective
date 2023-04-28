5499b598d6b52e37872627b57114f6b3d73e66f7
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-04-28 09:03:15 +0000

    Add LoginRequiredMixin to RecipeCreateView and return reverse to recipe-detail on recipe model

--- cookbook/models.py
+++ cookbook/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.urls import reverse
 
 
 STATUS = ((0, "Private"), (1, "Public"))
@@ -36,3 +37,9 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.title
+
+    
+    def get_absolute_url(self):
+        return reverse('recipe-detail', kwargs={'pk': self.pk})
+
+
