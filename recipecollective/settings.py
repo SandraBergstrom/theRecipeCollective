356ef0e70331154eb596080538cecdd880e545cd
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-05-10 13:25:53 +0000

    Change back static files storage

--- recipecollective/settings.py
+++ recipecollective/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'cloudinary_storage.storage.HashedCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
